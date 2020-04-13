b8f9f48d4f9b4669b2fd4ab601386da3d96d6154
Mitchell Gouzenko <mgouzenko@gmail.com>
2020-04-13 04:24:44 +0000

    Initial ansible configs to install neovim. Refactored dotfiles to subdirs

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ if ! [ -x "$(command -v ansible)" ]; then
   sudo apt-get update
   sudo apt-get -y install ansible
 fi
+
+ansible-playbook -i ./ansible/hosts ./ansible/install.yml --ask-become-pass --ssh-extra-args='-o "StrictHostKeyChecking no"'
