420d7146b55c5c8794f4d4f474d6805eebf3de05
Mitchell Gouzenko <mgouzenko@gmail.com>
2021-04-16 22:25:13 +0000

    stuff

--- install.sh
+++ install.sh
@@ -3,15 +3,6 @@ set -e
 
 CONFIGS=$HOME/configs
 
-# Install Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
-
-# Install antigen
-curl -L git.io/antigen > $HOME/.zsh/antigen.zsh
-
-# Install command line prompt
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure" || true
-
 ansible-playbook -i $CONFIGS/ansible/hosts $CONFIGS/ansible/install.yml \
 	--ask-become-pass \
 	--ssh-extra-args='-o "StrictHostKeyChecking no"' \
