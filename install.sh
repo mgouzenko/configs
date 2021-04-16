76df925b42145eb6dccfacff4489fcb953b802f5
Mitchell Gouzenko <mgouzenko@gmail.com>
2021-04-16 06:56:36 +0000

    tweak install script

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -e "$HOME/configs" ]; then
-  echo "Exists: $HOME/configs"
+CONFIGS=$HOME/configs
+
+if [ -e "$CONFIGS" ]; then
+  echo "Exists: $CONFIGS"
   exit 1
 fi
 
-git clone https://www.github.com/mgouzenko/configs.git configs
+git clone https://www.github.com/mgouzenko/configs.git $CONFIGS
 
 if ! [ -x "$(command -v ansible)" ]; then
   sudo apt-get update
   sudo apt-get -y install ansible
 fi
 
-ansible-playbook -i ./ansible/hosts ./ansible/install.yml --ask-become-pass --ssh-extra-args='-o "StrictHostKeyChecking no"'
+ansible-playbook -i $CONFIGS/ansible/hosts $CONFIGS/ansible/install.yml --ask-become-pass --ssh-extra-args='-o "StrictHostKeyChecking no"'
