76d4cd97d785349c9f625f63282122b050751443
Mitchell Gouzenko <mgouzenko@gmail.com>
2021-04-17 05:42:41 +0000

    refactor galaxy command

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,8 @@ if ! [ -x "$(command -v ansible)" ]; then
   sudo apt-get install software-properties-common
   sudo apt-add-repository --yes --update ppa:ansible/ansible
   sudo apt-get -y install ansible
-  ansible-galaxy collection install community.crypto
 fi
+
+ansible-galaxy collection install community.crypto
 
 $CONFIGS/install.sh
