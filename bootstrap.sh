c5063835ac95aaf4ebf8750c404e40ca71f26539
Mitchell Gouzenko <mgouzenko@gmail.com>
2022-01-16 16:16:50 -0500

    added more base packages and fixed up bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ if [ -e "$CONFIGS" ]; then
 fi
 
 git clone https://www.github.com/mgouzenko/configs.git $CONFIGS
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get -y install ansible
 $CONFIGS/install.sh
