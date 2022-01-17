b2e6a5f501c727e7a6d5ec3b6b8c002456bd7cbc
Mitchell Gouzenko <mgouzenko@gmail.com>
2022-01-17 16:23:27 -0500

    fixed install script to take default username

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,8 @@ fi
 
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get -y install git python3
-python3 -m pip install --user ansible
+sudo apt-get -y install git python3 python3-pip
+python3 -m pip install ansible
 
 git clone https://www.github.com/mgouzenko/configs.git $CONFIGS
 
--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 set -e
 
 CONFIGS=$HOME/configs
+DEFAULT_USER="gouzenko"
 
 ansible-playbook -i $CONFIGS/ansible/hosts $CONFIGS/ansible/install.yml \
 	--ask-become-pass \
 	--ssh-extra-args='-o "StrictHostKeyChecking no"' \
-	--extra-vars="dotfiles=$CONFIGS/config user=$USER"
+	--extra-vars="dotfiles=$CONFIGS/config user=${USER-$DEFAULT_USER}"
 
 exec zsh
