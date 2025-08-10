8041fe9c007e55733b5cfe1509698ceacb012253
Mitchell Gouzenko <mgouzenko@gmail.com>
2025-08-10 13:51:08 -0400

    Added logout script

---
+++ scripts/lock.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+options="Lock\nLogout\nReboot\nShutdown"
+
+chosen=$(
+zenity --question \
+  --switch \
+  --title="" \
+  --text="Select an exit option:" \
+  --extra-button="Log Out" \
+  --extra-button="Suspend" \
+  --extra-button="Reboot" \
+  --extra-button="Shutdown")
+
+case "$chosen" in
+    "Log Out")
+        i3-msg exit
+        ;;
+    "Suspend")
+        i3lock -i ~/configs/config/wallpapers/wallpaper.png & systemctl suspend
+        ;;
+    "Reboot")
+        systemctl reboot
+        ;;
+    "Shutdown")
+        systemctl poweroff
+        ;;
+esac
